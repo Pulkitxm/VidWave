4c33443309eeaeaa682ffd22ed825d6fa023f061
Pulkit <kpulkit15234@gmail.com>
2024-08-10 23:01:52 +0530

    transcoding and then uploading via docker

--- packages/docker-video/src/aws/functions.ts
+++ packages/docker-video/src/aws/functions.ts
@@ -5,7 +5,7 @@ import { PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
 import { v4 as uuid } from "uuid";
 import { AWS_BUCKET_NAME } from "../constants";
 
-export async function uploadObject(pathToFile: string){
+export async function uploadObject(id: string, pathToFile: string) {
   if (!fs.existsSync(pathToFile)) {
     throw new Error(`File not found: ${pathToFile}`);
   }
@@ -16,12 +16,10 @@ export async function uploadObject(pathToFile: string){
 
   const uploadParams: PutObjectCommandInput = {
     Bucket: AWS_BUCKET_NAME,
-    Key: uniqueFileName,
+    Key: id + "/" + uniqueFileName,
     Body: fileStream,
   };
 
   const command = new PutObjectCommand(uploadParams);
   await s3.send(command);
-
-  console.log(uploadParams.Key);
 }
--- packages/docker-video/src/index.ts
+++ packages/docker-video/src/index.ts
@@ -1,6 +1,18 @@
 import { uploadObject } from "./aws/functions";
 import { VIDEO_FILE_PATH } from "./constants";
+import { transCodeVideo } from "./ffmpeg";
+import fs from "fs";
+import { v4 as uuid } from "uuid";
 
 (async () => {
-  uploadObject(VIDEO_FILE_PATH);
+  const id = uuid();
+  console.log("Transcoding started");
+  const videos = await transCodeVideo(VIDEO_FILE_PATH);
+  console.log("Transcoding finished");
+  videos.forEach(async (video) => {
+    console.log(`Uploading ${video}`);
+    await uploadObject(id, video);
+    console.log(`Uploaded ${video}`);
+    fs.unlinkSync(video);
+  });
 })();
